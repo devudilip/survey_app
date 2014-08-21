4c06963a74579b33625961558e06e96402d425f0
devudilip <devu.dilip@gmail.com>
2014-08-21 23:59:28 +0530

    first commit with all needed gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyApp::Application.config.secret_token = '1c28c9aa3c43debf40adb18a84075f80c7431365b837d2ba38137606f010455e5f4c750decd602e0f56e8733f9fb4ebfd572e4e447c59aa01ced9a99396b39e7'
