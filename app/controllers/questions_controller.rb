d66bf613a5635c8784a846905c4f4873ac2d655f
devudilip <devu.dilip@gmail.com>
2014-08-22 02:23:27 +0530

    questions new and create action added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,23 @@
 class QuestionsController < ApplicationController
-  
+
   def index
     @survey = Survey.find params[:survey_id]
     @questions = @survey.questions
+  end
+  
+  def new
+    @survey = Survey.find params[:survey_id]
+    @question = @survey.questions.new
+  end
+
+  def create
+    @survey = Survey.find params[:survey_id]
+    @question = @survey.questions.new(params[:question])
+    if @question.save
+      redirect_to surveys_path
+    else
+      render 'new'
+    end
   end
 
 end
