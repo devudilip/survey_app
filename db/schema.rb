ef9c596dc0016748576a419955ecbbe16b1e1045
devudilip <devu.dilip@gmail.com>
2014-08-22 00:55:19 +0530

    survey model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140821191841) do
+ActiveRecord::Schema.define(:version => 20140821192403) do
+
+  create_table "surveys", :force => true do |t|
+    t.string   "name",       :default => "", :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
