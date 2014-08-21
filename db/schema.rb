7021f45589184e41952721612f50126243702323
devudilip <devu.dilip@gmail.com>
2014-08-22 01:51:24 +0530

    custom field for questions and seed added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140821192403) do
+ActiveRecord::Schema.define(:version => 20140821201812) do
+
+  create_table "custom_fields", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "surveys", :force => true do |t|
     t.string   "name",       :default => "", :null => false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# seed to add predefined data types 
+fields =  ["Essay","Numeric","Date","Radio Button","Check Box"]
+fields.collect{ |field| CustomField.create(name: field) }
