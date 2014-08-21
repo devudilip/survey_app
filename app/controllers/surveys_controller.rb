5dde5030e23bdf3c1f089dd486e100cf551224af
devudilip <devu.dilip@gmail.com>
2014-08-22 01:29:17 +0530

    surveys crud

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -4,4 +4,44 @@ class SurveysController < ApplicationController
     @surveys = Survey.all
   end
 
+  def show
+    @survey = Survey.find params[:id]
+  end
+
+  def new
+    @survey = Survey.new
+  end
+
+  def edit
+    @survey = Survey.find params[:id]
+  end
+
+  def create
+    @survey = Survey.new params[:survey]
+    if @survey.save
+      flash[:notice] = 'Survey successfully created.'
+      redirect_to surveys_path 
+    else
+      flash[:error] = 'Survey creation failed.'
+      render "new" 
+    end
+  end
+
+  def update
+    @survey = Survey.find params[:id]
+    if @survey.update_attributes params[:survey]
+      flash[:notice] = 'Survey successfully updated.'
+      redirect_to surveys_path
+    else
+      flash[:error] = 'Survey update failed.'
+      render "edit"
+    end
+  end
+
+  def destroy
+    @survey = Survey.find params[:id]
+    @survey.destroy
+    redirect_to surveys_path
+  end
+
 end
