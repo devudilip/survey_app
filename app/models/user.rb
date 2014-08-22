842535bcbfd32c3535d6b6a68691f379bf357cc2
devudilip <devu.dilip@gmail.com>
2014-08-22 20:56:33 +0530

    devise views user name field aded for sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :user_name
   # attr_accessible :title, :body
+  validates :user_name, presence: true, :allow_blank => false, :uniqueness => { :case_sensitive => false }
+
 end
