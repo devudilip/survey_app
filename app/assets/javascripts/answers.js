49863267377130ce7b937498e25b0bfe41b97518
devudilip <devu.dilip@gmail.com>
2014-08-22 21:50:14 +0530

    validation messages and date picker

---
+++ app/assets/javascripts/answers.js
@@ -0,0 +1,46 @@
+$( document ).ready( function () {
+
+
+	$('#new_answer').validate({
+		
+		highlight: function(element) {
+			$(element).closest('.control-group').removeClass('success').addClass('error');
+		},
+		success: function(element) {
+			element
+			.addClass('valid')
+			.closest('.control-group').removeClass('error').addClass('success');
+		}
+	});
+
+	$( ".required" ).each(function(){
+		$(this).rules( "add", {
+			required: true,
+			messages: {
+				required: "Can't be blank"
+			}
+		});
+	});
+
+	$( ".survey_number" ).each(function(){
+		$(this).rules( "add", {
+			number: true,
+			messages: {
+				number: "Should be number"
+			}
+		});
+	});
+
+	$( ".survey_date" ).each(function(){
+		$(this).rules( "add", {
+			date: true,
+			messages: {
+				date: "Should be date"
+			}
+		});
+	});
+	
+
+	$('.survey_date').datepicker()
+
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,20 +2,20 @@ module ApplicationHelper
 
   def get_field_tag question
     if question.custom_field.name == "Numeric"
-      tag("input", type: 'number', name:  "survey[#{question.id}][]")
+      tag("input", type: 'number', name:  "survey[#{question.id}][]", class: "survey_number required")
     elsif question.custom_field.name == "Essay"
-      tag("input", type: 'text_area', name:  "survey[#{question.id}][]", class: "survey_area")
+      content_tag(:textarea, "", name:  "survey[#{question.id}][]", class: "survey_area required")
     elsif question.custom_field.name == "Radio Button"
       radio = ''
       options = question.options.split("\n")
-      options.collect{ |text| radio += tag("input", type: 'radio', name:  "survey[#{question.id}][]", value: text)  + text }
+      options.collect{ |text| radio += tag("input", type: 'radio', name:  "survey[#{question.id}][]", value: text, class: "survey_radio required")  + text }
       return raw(radio)
     elsif question.custom_field.name == "Date"
-      tag("input", type: 'date', name:  "survey[#{question.id}][]")
+      tag("input", type: 'date', name:  "survey[#{question.id}][]", class: "survey_date required", autocomplete: "off")
     elsif  question.custom_field.name == "Check Box" 
       checkbox = ''
       options = question.options.split("\n")
-      options.collect{ |text| checkbox += tag("input", type: 'checkbox', name:  "survey[#{question.id}][]", value: text)  + text }
+      options.collect{ |text| checkbox += tag("input", type: 'checkbox', name:  "survey[#{question.id}][]", value: text, class: "survey_check_box required")  + text }
       return raw(checkbox)
     end
   end
