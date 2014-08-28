ab0289d268014729d56ee6f7767557edfca58676
devudilip <devu.dilip@gmail.com>
2014-08-28 15:18:05 +0530

    Spec Green for surveys

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,63 +4,62 @@ RSpec.describe QuestionsController, :type => :controller do
   let(:custom_field) {CustomField.create!(name: "Text")}
   let(:survey) { FactoryGirl.build(:survey) }
   let(:valid_attributes) {
-    skip({name: "Test Survey", custom_field_id: custom_field.id, survey_id: survey.id })
-  }
+   {name: "Test Survey", custom_field_id: custom_field.id, survey_id: survey.id }
+ }
 
-  let(:invalid_attributes) {
-    skip({name: "", custom_field_id: custom_field.id})
-  }
-  context "Logged In user" do
-    before { sign_in FactoryGirl.build(:user) }
+ let(:invalid_attributes) {
+  {name: "", custom_field_id: custom_field.id}
+}
+context "Logged In user" do
+  before { sign_in FactoryGirl.build(:user) }
 
-    describe "GET index" do
-      it "assigns all questions to @surveys which is related to @survey" do
+  describe "GET index" do
+    it "assigns all questions to @surveys which is related to @survey" do
+      question = survey.questions.create! valid_attributes
+      get :index, {}
+      expect(assigns(:questions)).to eq([question])
+    end
+  end
+
+  describe "GET new" do
+    it "assigns a new survey.question as @question" do
+      get :new, {:survey_id => 1}
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+  end
+
+  describe "POST create" do
+    describe "with valid params" do
+      it "creates a new question" do
         question = survey.questions.create! valid_attributes
-        get :index, {}
-        expect(assigns(:questions)).to eq([question])
+        post :create, {}
+        expect(question).to change(question, :count).by(1)
+      end
+
+      it "assigns a newly created question as @question" do
+        post :create, {:question => valid_attributes}
+        expect(assigns(:question)).to be_a(question)
+        expect(assigns(:question)).to be_persisted
+      end
+
+      it "redirects to the created question" do
+        post :create, {:question => valid_attributes}
+        expect(response).to redirect_to(surveys_path)
       end
     end
 
-    describe "GET new" do
-      xit "assigns a new survey.question as @question" do
-        question = survey.questions.new
-        get :new, {}
-        expect(assigns(:question)).to eq([question])
+    describe "with invalid params" do
+      it "assigns a newly created but unsaved question as @question" do
+        post :create, {:question => invalid_attributes}
+        expect(assigns(:question)).to be_a_new(question)
+      end
+
+      it "re-renders the 'new' template" do
+        post :create, {:question => invalid_attributes}
+        expect(response).to render_template("new")
       end
     end
+  end
 
-    describe "POST create" do
-      describe "with valid params" do
-        it "creates a new question" do
-          question = survey.questions.create! valid_attributes
-          post :create, {}
-          expect(question).to change(question, :count).by(1)
-        end
-
-        it "assigns a newly created question as @question" do
-          post :create, {:question => valid_attributes}
-          expect(assigns(:question)).to be_a(question)
-          expect(assigns(:question)).to be_persisted
-        end
-
-        it "redirects to the created question" do
-          post :create, {:question => valid_attributes}
-          expect(response).to redirect_to(surveys_path)
-        end
-      end
-
-      describe "with invalid params" do
-        it "assigns a newly created but unsaved question as @question" do
-          post :create, {:question => invalid_attributes}
-          expect(assigns(:question)).to be_a_new(question)
-        end
-
-        it "re-renders the 'new' template" do
-          post :create, {:question => invalid_attributes}
-          expect(response).to render_template("new")
-        end
-      end
-    end
-
-  end    
+end    
 end
--- spec/controllers/surveys_controller_spec.rb
+++ spec/controllers/surveys_controller_spec.rb
@@ -1,137 +1,142 @@
 require 'rails_helper'
 
-RSpec.describe SurveysController, :type => :controller do
+describe SurveysController, :type => :controller do
+
   let(:valid_attributes) {
-    skip({name: "Test Survey"})
+    {name: "Test Survey"}
   }
 
   let(:invalid_attributes) {
-    skip({name: ""})
+    {name: ""}
   }
 
   context "Admin login" do
-    before { sign_in FactoryGirl.build(:user) }
+    before do
+      @user = FactoryGirl.build(:user)
+      sign_in @user
+      @abilities = Ability.new(@user)
+      Ability.stub(:new).and_return(@abilities)
+    end
 
-    describe "GET index" do
-      it "assigns all surveys to @surveys" do
+
+  describe "GET index" do
+    it "assigns all surveys to @surveys" do
+      survey = Survey.create! valid_attributes
+      get :index
+      expect(assigns(:surveys)).to eq([survey])
+    end
+  end
+
+
+  describe "GET show" do
+    it "Assigns object to @survey" do
+      survey = Survey.create! valid_attributes
+      get :show, {:id => survey.id}
+      expect(assigns(:survey)).to eq(survey)
+    end
+  end
+
+  describe "GET new" do
+    it "assigns a new survey as @survey" do
+      get :new
+      expect(assigns(:survey)).to be_a_new(Survey)
+    end
+  end
+
+  describe "GET edit" do
+    it "assigns the requested survey as @survey" do
+      survey = Survey.create! valid_attributes
+      get :edit, {:id => survey.to_param}
+      expect(assigns(:survey)).to eq(survey)
+    end
+  end
+
+  describe "POST create" do
+    describe "with valid params" do
+      it "creates a new survey" do
+        post :create
+        expect { survey = Survey.create! valid_attributes }.to change{ Survey.count }.by(1)
+      end
+
+      it "assigns a newly created survey as @survey" do
+        post :create, {:survey => valid_attributes}
+        expect(assigns(:survey)).to be_a(Survey)
+        expect(assigns(:survey)).to be_persisted
+      end
+
+      it "redirects to the created survey" do
+        post :create, {:survey => valid_attributes}
+        expect(response).to redirect_to(surveys_path)
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns a newly created but unsaved survey as @survey" do
+        post :create, {:survey => invalid_attributes}
+        expect(assigns(:survey)).to be_a_new(Survey)
+      end
+
+      it "re-renders the 'new' template" do
+        post :create, {:survey => invalid_attributes}
+        expect(response).to render_template("new")
+      end
+    end
+  end
+  describe "PUT update" do
+    describe "with valid params" do
+      let(:new_attributes) {
+        {name: "Changed Name"}
+      }
+
+      it "updates the requested survey" do
         survey = Survey.create! valid_attributes
-        get :index, {}
-        expect(assigns(:surveys)).to eq([survey])
+        put :update, {:id => survey.to_param, :survey => new_attributes}
+
       end
-    end
 
-
-    describe "GET show" do
-      it "Assigns object to @survey" do
-        survey = Survey.create! valid_attributes
-        get :show, {:id => survey.id}
-        expect(assigns(:survey)).to eq(survey)
-      end
-    end
-
-    describe "GET new" do
-      xit "assigns a new survey as @survey" do
-        # survey = Survey.new
-        get :new, {}
-        expect(assigns(:survey)).to be_a_new(survey)
-      end
-    end
-
-    describe "GET edit" do
       it "assigns the requested survey as @survey" do
         survey = Survey.create! valid_attributes
-        get :edit, {:id => survey.to_param}
+        put :update, {:id => survey.to_param, :survey => valid_attributes}
         expect(assigns(:survey)).to eq(survey)
       end
-    end
 
-    describe "POST create" do
-      describe "with valid params" do
-        xit "creates a new survey" do
-          expect {
-            post :create, {:survey => valid_attributes}
-            }.to change(survey, :count).by(1)
-          end
-
-          it "assigns a newly created survey as @survey" do
-            post :create, {:survey => valid_attributes}
-            expect(assigns(:survey)).to be_a(survey)
-            expect(assigns(:survey)).to be_persisted
-          end
-
-          it "redirects to the created survey" do
-            post :create, {:survey => valid_attributes}
-            expect(response).to redirect_to(surveys_path)
-          end
-        end
-
-        describe "with invalid params" do
-          it "assigns a newly created but unsaved survey as @survey" do
-            post :create, {:survey => invalid_attributes}
-            expect(assigns(:survey)).to be_a_new(survey)
-          end
-
-          it "re-renders the 'new' template" do
-            post :create, {:survey => invalid_attributes}
-            expect(response).to render_template("new")
-          end
-        end
-      end
-      describe "PUT update" do
-        describe "with valid params" do
-          let(:new_attributes) {
-            skip({name: "Changed Name"})
-          }
-
-          it "updates the requested survey" do
-            survey = Survey.create! valid_attributes
-            put :update, {:id => survey.to_param, :survey => new_attributes}
-            
-          end
-
-          it "assigns the requested survey as @survey" do
-            survey = Survey.create! valid_attributes
-            put :update, {:id => survey.to_param, :survey => valid_attributes}
-            expect(assigns(:survey)).to eq(survey)
-          end
-
-          it "redirects to the survey" do
-            survey = Survey.create! valid_attributes
-            put :update, {:id => survey.to_param, :survey => valid_attributes}
-            expect(response).to redirect_to(surveys_path)
-          end
-        end
-
-        describe "with invalid params" do
-          it "assigns the survey as @survey" do
-            survey = Survey.create! valid_attributes
-            put :update, {:id => survey.to_param, :survey => invalid_attributes}
-            expect(assigns(:survey)).to eq(survey)
-          end
-
-          it "re-renders the 'edit' template" do
-            survey = Survey.create! valid_attributes
-            put :update, {:id => survey.to_param, :survey => invalid_attributes}
-            expect(response).to render_template("edit")
-          end
-        end
-      end
-
-      describe "DELETE destroy" do
-        it "destroys the requested survey" do
-          survey = Survey.create! valid_attributes
-          expect {
-            delete :destroy, {:id => survey.to_param}
-            }.to change(survey, :count).by(-1)
-          end
-
-          it "redirects to the surveys list" do
-            survey = Survey.create! valid_attributes
-            delete :destroy, {:id => survey.to_param}
-            expect(response).to redirect_to(surveys_path)
-          end
-        end
-
-
+      it "redirects to the survey" do
+        survey = Survey.create! valid_attributes
+        put :update, {:id => survey.to_param, :survey => valid_attributes}
+        expect(response).to redirect_to(surveys_path)
       end
     end
+
+    describe "with invalid params" do
+      it "assigns the survey as @survey" do
+        survey = Survey.create! valid_attributes
+        put :update, {:id => survey.to_param, :survey => invalid_attributes}
+        expect(assigns(:survey)).to eq(survey)
+      end
+
+      it "re-renders the 'edit' template" do
+        survey = Survey.create! valid_attributes
+        put :update, {:id => survey.to_param, :survey => invalid_attributes}
+        expect(response).to render_template("edit")
+      end
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "destroys the requested survey" do
+      survey = Survey.create! valid_attributes
+      expect {
+        delete :destroy, {:id => survey.to_param}
+        }.to change(Survey, :count).by(-1)
+      end
+
+      it "redirects to the surveys list" do
+        survey = Survey.create! valid_attributes
+        delete :destroy, {:id => survey.to_param}
+        expect(response).to redirect_to(surveys_path)
+      end
+    end
+
+
+  end
+end
