2800e12d6eadcb8cf2e0c071a330b742dbe4e5dd
devudilip <devu.dilip@gmail.com>
2014-08-28 20:54:30 +0530

    uncomment line

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,7 +3,7 @@ class Survey < ActiveRecord::Base
   has_many :questions, dependent: :destroy
   has_many :answers,dependent: :destroy
   has_many :survey_users
-  has_many :users, through: :survey_users,dependent: :destroy#, :uniq => true
+  has_many :users, through: :survey_users,dependent: :destroy, :uniq => true
 
   validates :name, presence: true
 end
